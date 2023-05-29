7ee7804f7a23888184de975cd6f77515ece10189
Dam Nock <daminnock@gmail.com>
2023-05-29 20:07:28 -0300

    Push already working

--- contracts/SCADA.sol
+++ contracts/SCADA.sol
@@ -242,15 +242,15 @@ contract SCADA {
                 address(this), // to recipient, put address(this) in case you want Broadcast or Subset. For Targetted put the address to which you want to send
                 bytes(
                     string(
-                        // We are passing identity here: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                        // https://docs.push.org/developers/developer-guides/sending-notifications/notification-payload-types/notification-standard-basics
                         abi.encodePacked(
-                            "0", // this is notification identity: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                            "0", // this is notification identity: 0 recommended for smart contracts
                             "+", // segregator
-                            "3", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
+                            "1", // this is payload type: https://docs.push.org/developers/developer-guides/sending-notifications/notification-payload-types (1, 3 or 4) = (Broadcast, targetted or subset)
                             "+", // segregator
                             "New event", // this is notification title
                             "+", // segregator
-                            _message
+                           _message
                         )
                     )
                 )
